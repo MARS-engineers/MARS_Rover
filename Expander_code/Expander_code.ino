401274ff0d2730a86676ee39d5d708a858d32552
scratchbit <matejmrkva@seznam.cz>
2025-05-03 21:35:39 +0200

    Test code

--- Expander_code/Expander_code.ino
+++ Expander_code/Expander_code.ino
@@ -8,9 +8,13 @@
 void setup() {
   // put your setup code here, to run once:
 
+  // Blink at start
+  pinMode(EXP_LED, OUTPUT);
+  digitalWriteFast(EXP_LED, HIGH);
+  delay(100);
+  digitalWriteFast(EXP_LED, LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
--- Expander_code/config.h
+++ Expander_code/config.h
@@ -1,1 +1,3 @@
 // Configuration
+
+#define EXP_LED
